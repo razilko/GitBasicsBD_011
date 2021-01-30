53d32cb8667e7dcee51c4a7935e4eacceef6453f
razilko <72981823+razilko@users.noreply.github.com>
2021-01-30 23:23:02 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int Sum(int n/*Кол-во передаваемых аргументов*/, int value /*Первый аргумент*/, ...)
+{
+	int sum = 0;
+	int* start = &value;
+	for (int i = 0; i < n; i++)
+	{
+		sum += *start++;
+	}
+	return sum;
+}
+
+int Mul(int value ...)
+{
+	int product = 1;
+	int* start = &value;
+	while (*start != int())
+		product *= *start++;
+	return product;
+}
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Sum = "<< Sum(6, 2, 3, 5, 8, 13, 21) << endl;
+	cout << "Mul = " << Mul(6, 2, 3, 5, 8, 13, 21, 0) << endl;
+}
